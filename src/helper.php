bbcbd05ccbee1cdfb254609dd1424ccf936ddd69
lxh <1450612626@qq.com>
2019-04-22 16:08:18 +0800

    t

--- src/helper.php
+++ src/helper.php
@@ -77,3 +77,20 @@ function getOldMonth($lenth = 1){
     }
     return array_reverse($array);
 }
+
+/**
+ * 过滤微信昵称特殊字符
+ * @param $str
+ * @return string|string[]|null
+ */
+function filterEmoji($str)
+    {
+        $str = preg_replace_callback(
+            '/./u',
+            function (array $match) {
+                return strlen($match[0]) >= 4 ? '' : $match[0];
+            },
+            $str);
+
+        return $str;
+    }
